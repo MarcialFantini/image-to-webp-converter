1d117cc94a94819ca87f5c6233264d77bccea9ab
MarcialFantini <marcialfantini@gmail.com>
2024-07-06 18:11:56 -0300

    complete

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import imageCompression from "browser-image-compression";
+import "./App.css";
+import { useDropzone } from "react-dropzone";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+function App() {
+  const [files, setFiles] = useState([]);
+  const [webpFiles, setWebpFiles] = useState([]);
+
+  const onDrop = (acceptedFiles) => {
+    setFiles([...files, ...acceptedFiles]);
+  };
+
+  const convertToWebP = async () => {
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 800,
+      useWebWorker: true,
+      fileType: "image/webp",
+    };
+
+    const promises = files.map(async (file) => {
+      try {
+        const compressedFile = await imageCompression(file, options);
+        return compressedFile;
+      } catch (error) {
+        console.error("Error during compression:", error);
+        return null;
+      }
+    });
+
+    const convertedFiles = await Promise.all(promises);
+    setWebpFiles(convertedFiles.filter((file) => file !== null));
+  };
+
+  const downloadAll = async () => {
+    const zip = new JSZip();
+    webpFiles.forEach((file, index) => {
+      zip.file(`image-${index}.webp`, file);
+    });
+
+    const content = await zip.generateAsync({ type: "blob" });
+    saveAs(content, "converted_images.zip");
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: "image/*",
+  });
+
+  return (
+    <div className="App">
+      <h1>Convertidor de Imágenes a WebP</h1>
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>
+          Arrastra y suelta tus imágenes aquí, o haz clic para seleccionar
+          archivos
+        </p>
+        <div className="preview-container">
+          {files.map((file, index) => (
+            <div key={index} className="preview-item">
+              <img src={URL.createObjectURL(file)} alt={`preview ${index}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <button onClick={convertToWebP}>Convertir a WebP</button>
+      {webpFiles.length > 0 && (
+        <button onClick={downloadAll}>Descargar todas</button>
+      )}
+      <div className="images-container">
+        {webpFiles.map((file, index) => (
+          <div key={index} className="image-item">
+            <h2>Imagen Convertida:</h2>
+            <img src={URL.createObjectURL(file)} alt={`Converted ${index}`} />
+            <a
+              href={URL.createObjectURL(file)}
+              download={`converted-${index}.webp`}
+            >
+              Descargar
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
